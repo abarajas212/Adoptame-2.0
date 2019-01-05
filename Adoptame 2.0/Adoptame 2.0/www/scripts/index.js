765968ce89063dc448a293de053cf1c278a1fb3c
abarajas212 <a_barajas212@usal.es>
2019-01-05 01:49:47 +0100

    Limpieza código HTML

--- Adoptame 2.0/Adoptame 2.0/www/scripts/index.js
+++ Adoptame 2.0/Adoptame 2.0/www/scripts/index.js
@@ -37,8 +37,8 @@ document.addEventListener('deviceready', onDeviceReady.bind(this), false);
             // ... other parameters
         });
 
-        window.sessionStorage.setItem("IP", "192.168.1.128");
-        ip = "192.168.1.128";
+        window.sessionStorage.setItem("IP", "192.168.1.129");
+        ip = "192.168.1.129";
 
         //Comprobar si hay usuario guardado y su contrasenia
         usuarioDispositivo = window.localStorage.getItem("usuarioDispositivo");
